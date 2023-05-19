9edff19eac35f50fea3424604c041c1618025827
Diego Luna <elotrodiegoluna@gmail.com>
2023-05-19 19:39:03 -0400

    PA db

--- core/settings.py
+++ core/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-##        'NAME': 'bibliomanga',
-#        'USER': 'root',
-#        'PASSWORD': '1234',
- #       'HOST': 'localhost',
-#        'PORT': '3306',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elotrodiego$bibliomanga',
+        'USER': 'elotrodiego',
+        'PASSWORD': 'bibliomanga',
+        'HOST': 'elotrodiego.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
